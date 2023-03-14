510c82093ffe801c43a1d4cd41a775bceb243316
Raudy0 <rrosales@broncs.utpa.edu>
2023-03-14 15:44:54 -0500

    co

--- theme.js
+++ theme.js
@@ -27,3 +27,8 @@ const map = new google.maps.Map(document.getElementById("map"), {
   zoom: 4,
   center: uluru,
 });
+// The marker, positioned at Uluru
+const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
