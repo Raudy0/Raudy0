8b9b4a857d9f2ff1221951b248a90e5d9f234467
Raudy0 <rrosales@broncs.utpa.edu>
2023-03-14 15:43:32 -0500

    commit

--- theme.js
+++ theme.js
@@ -20,3 +20,10 @@ function toggleTheme() {
 }
 
 document.getElementById('themeButton').onclick = toggleTheme;
+// The location of Uluru
+const uluru = { lat: -25.344, lng: 131.031 };
+// The map, centered at Uluru
+const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 4,
+  center: uluru,
+});
